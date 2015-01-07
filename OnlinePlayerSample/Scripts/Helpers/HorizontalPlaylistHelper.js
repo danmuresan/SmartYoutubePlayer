7c93355c417badd71c4489d012793d884c4c62e7
Dan Muresan <dan.muresan@ullink.com>
2015-01-07 14:50:13 +0200

    Playlist sample
    Added tile based sample playlist plus other minor stuff

---
+++ OnlinePlayerSample/Scripts/Helpers/HorizontalPlaylistHelper.js
@@ -0,0 +1,14 @@
+﻿$(document).ready(function () {
+    $(".arrow-right").bind("click", function (event) {
+        event.preventDefault();
+        $(".vid-list-container").stop().animate({
+            scrollLeft: "+=336"
+        }, 750);
+    });
+    $(".arrow-left").bind("click", function (event) {
+        event.preventDefault();
+        $(".vid-list-container").stop().animate({
+            scrollLeft: "-=336"
+        }, 750);
+    });
+});
