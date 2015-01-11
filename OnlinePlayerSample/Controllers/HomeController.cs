cf5e3e9d3041e3c1a0069555a8b9d2b6a494da77
danmuresan <dan91142006@yahoo.com>
2015-01-11 17:52:32 +0200

    Added login and register functionality
    Plus design changes

--- OnlinePlayerSample/Controllers/HomeController.cs
+++ OnlinePlayerSample/Controllers/HomeController.cs
@@ -4,11 +4,13 @@ namespace OnlinePlayerSample.Controllers
 {
     public class HomeController : Controller
     {
+        [AllowAnonymous]
         public ActionResult Index()
         {
             return View();
         }
 
+        [AllowAnonymous]
         public ActionResult About()
         {
             ViewBag.Message = "Your app description page.";
@@ -16,11 +18,13 @@ namespace OnlinePlayerSample.Controllers
             return View();
         }
 
+        [AllowAnonymous]
         public ActionResult YoutubeDemo()
         {
             return View();
         }
 
+        [AllowAnonymous]
         public ActionResult Login()
         {
             return PartialView("_Login");
